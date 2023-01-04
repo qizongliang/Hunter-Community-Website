1d38883f34c27dd0adc88869b46fdf9f119905eb
qizongliang <72285030+qizongliang@users.noreply.github.com>
2023-01-04 10:37:05 -0500

    Added Data for CS track 1, create more folder for organizing

---
+++ huntercommunitywebsite/src/DegreeMap/const/csTrack.js
@@ -0,0 +1,49 @@
+export const csTracks = [
+  {
+    id: 1,
+    precal: false,
+    summer: false,
+    track: [
+      {
+        id: 1,
+        name: 'Freshman Fall',
+        courses: ['CSCI 12700', 'MATH 12550'],
+      },
+      {
+        id: 2,
+        name: 'Freshman Spring',
+        courses: ['CSCI 13500', 'CSCI 15000', 'MATH 15000'],
+      },
+      {
+        id: 3,
+        name: 'Sophomore Fall',
+        courses: ['CSCI 16000', 'CSCI 23500', 'MATH 15500'],
+      },
+      {
+        id: 4,
+        name: 'Sophomore Spring',
+        courses: ['CSCI 26000', 'CSCI 33500', 'MATH 16000'],
+      },
+      {
+        id: 5,
+        name: 'Junior Fall',
+        courses: ['CSCI 26500', 'STAT 21300', 'CS Elective'],
+      },
+      {
+        id: 6,
+        name: 'Junior Spring',
+        courses: ['CSCI 34000', 'CS Elective'],
+      },
+      {
+        id: 7,
+        name: 'Senior Fall',
+        courses: ['CSCI 49900', 'CS Elective'],
+      },
+      {
+        id: 8,
+        name: 'Senior Spring',
+        courses: ['CS Elective'],
+      },
+    ],
+  },
+]
--- huntercommunitywebsite/src/NavBar.js
+++ huntercommunitywebsite/src/NavBar.js
@@ -1,24 +1,23 @@
-import React from "react";
-import { AppBar, Toolbar, Typography, Tab, Tabs } from "@mui/material";
-const Header = () => 
-{
-    return(
-        <React.Fragment>
-            <AppBar>
-                <Toolbar>
-                    <Typography> HUNTER CS GUIDE </Typography>
-                    <Tabs textColor="inherit">
-                        <Tab label= "About" />
-                        <Tab label= "Degree Map" />
-                        <Tab label= "Internships"/>
-                        <Tab label= "Authors" />
-                        
-                    </Tabs>
+import React from 'react'
+import { AppBar, Toolbar, Typography, Tab, Tabs } from '@mui/material'
 
-                </Toolbar>
-            </AppBar>
-        </React.Fragment>
-    )
+const Header = () => {
+  return (
+    <React.Fragment>
+      <AppBar>
+        <Toolbar>
+          <Typography> HUNTER CS GUIDE </Typography>
+          <Tabs textColor="inherit">
+            <Tab label="ABOUT" />
+            <Tab label="DEGREE MAP" />
+            <Tab label="INTERNSHIP GUIDE" />
+            <Tab label="RESOURCES" />
+            <Tab label="TEAM" />
+          </Tabs>
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  )
 }
 
-export default Header;
+export default Header
