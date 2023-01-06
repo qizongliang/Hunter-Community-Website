86323ea0a192256974a7ff361b5edceca3c8ca06
Pramit-i8 <pramitdey22@gmail.com>
2023-01-05 22:03:20 -0500

    Populated degreemaps

--- huntercommunitywebsite/src/DegreeMap/const/csTrack.js
+++ huntercommunitywebsite/src/DegreeMap/const/csTrack.js
@@ -1,7 +1,7 @@
 export const csTracks = [
   {
     id: 1,
-    precal: false,
+    precal_passed: false,
     summer: false,
     track: [
       {
@@ -43,6 +43,173 @@ export const csTracks = [
         id: 8,
         name: 'Senior Spring',
         courses: ['CS Elective'],
+      },
+    ],
+  },
+  
+  {
+    id: 2,
+    precal_passed: false,
+    summer: true,
+    track: 
+    [
+      {
+        id: 1,
+        name: 'Freshman Fall',
+        courses: ['CSCI 12700', 'MATH 12550'],
+      },
+      {
+        id: 2,
+        name: 'Freshman Spring',
+        courses: ['CSCI 13500', 'CSCI 15000'],
+      },
+      {
+        id: 3,
+        name: "Freshman Summer",
+        courses: ['CSCI 15000'],
+      },
+      {
+        id: 4,
+        name: 'Sophomore Fall',
+        courses: ['CSCI 16000', 'MATH 15500'],
+      },
+      
+      {
+        id: 5,
+        name: 'Sophomore Spring',
+        courses: ['CSCI 26000', 'CSCI 23500', 'MATH 16000'],
+      },
+      {
+        id: 6,
+        name: "Sophomore Summer",
+        courses: ['CSCI 26500']
+      },
+      {
+        id: 7,
+        name: 'Junior Fall',
+        courses: ['CSCI 33500','STAT 21300'],
+      },
+      {
+        id: 8,
+        name: 'Junior Spring',
+        courses: ['CSCI 34000', 'CS Elective'],
+      },
+      {
+        id: 9,
+        name: 'Senior Fall',
+        courses: ['CSCI 49900', 'CS Elective'],
+      },
+      {
+        id: 10,
+        name: 'Senior Spring',
+        courses: ['CS Elective','CS Elective'],
+      },
+    ],
+  },
+
+  {
+    id: 3,
+    precal_passed: true,
+    summer: false,
+    track: [
+      {
+        id: 1,
+        name: 'Freshman Fall',
+        courses: ['CSCI 12700', 'MATH 15000'],
+      },
+      {
+        id: 2,
+        name: 'Freshman Spring',
+        courses: ['CSCI 13500', 'CSCI 15500'],
+      },
+      {
+        id: 3,
+        name: 'Sophomore Fall',
+        courses: ['CSCI 23500', 'CSCI 15000'],
+      },
+      {
+        id: 4,
+        name: 'Sophomore Spring',
+        courses: ['CSCI 16000', 'MATH 16000'],
+      },
+      {
+        id: 5,
+        name: 'Junior Fall',
+        courses: ['CSCI 26500','STAT 21300'],
+      },
+      {
+        id: 6,
+        name: 'Junior Spring',
+        courses: ['CSCI 33500', 'CSCI 26000','CS Elective'],
+      },
+      {
+        id: 7,
+        name: 'Senior Fall',
+        courses: ['CSCI 34000', 'CS Elective'],
+      },
+      {
+        id: 8,
+        name: 'Senior Spring',
+        courses: ['CSCI 49900','CS Elective','CS Elective'],
+      },
+    ],
+  },
+  {
+    id: 4,
+    precal_passed: true,
+    summer: true,
+    track: 
+    [
+      {
+        id: 1,
+        name: 'Freshman Fall',
+        courses: ['CSCI 12700', 'MATH 15000'],
+      },
+      {
+        id: 2,
+        name: 'Freshman Spring',
+        courses: ['CSCI 13500', 'MATH 15500'],
+      },
+      {
+        id: 3,
+        name: "Freshman Summer",
+        courses: ['CSCI 15000'],
+      },
+      {
+        id: 4,
+        name: 'Sophomore Fall',
+        courses: ['CSCI 16000', 'MATH 16000'],
+      },
+      
+      {
+        id: 5,
+        name: 'Sophomore Spring',
+        courses: [ 'CSCI 23500', 'STAT 21300'],
+      },
+      {
+        id: 6,
+        name: "Sophomore Summer",
+        courses: ['CSCI 26500']
+      },
+      {
+        id: 7,
+        name: 'Junior Fall',
+        courses: ['CSCI 33500','CS Elective'],
+      },
+      {
+        id: 8,
+        name: 'Junior Spring',
+        courses: ['CSCI 26000', 'CS Elective'],
+      },
+      {
+        id: 9,
+        name: 'Senior Fall',
+        courses: ['CSCI 34000', 'CS Elective'],
+      },
+      {
+        id: 10,
+        name: 'Senior Spring',
+        courses: ['CSCI 49900','CS Elective'],
       },
     ],
   },
