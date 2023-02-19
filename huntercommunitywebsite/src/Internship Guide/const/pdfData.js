1d7583139a26cc5fcd6c42da59bcfd7abf2e6b95
qizongliang <72285030+qizongliang@users.noreply.github.com>
2023-02-18 20:56:06 -0500

    import all pdfs

---
+++ huntercommunitywebsite/src/Internship Guide/const/pdfData.js
@@ -0,0 +1,21 @@
+export const pdfInternshipData = [
+  {
+    id: 1,
+    title: 'Guide To Your First College Aide Job',
+    url: 'Guide To Your First College Aide Job.pdf',
+    description: `As a CUNY student, you have many exclusive resources for you to utilize. You have to
+        know what you want in order to really learn what to apply for. New York especially is an urban
+        jungle, full of opportunity to become successful. College Aide is one of the resources that can
+        help students to get work experience and pay. Potentially an internship that lasts until you’re
+        done with college. Is a great way to build connections too.`,
+  },
+  {
+    id: 2,
+    title: 'How To Find Beginner Internship As A CUNY Student',
+    url: 'How To Find Beginner Internship As A CUNY Student.pdf',
+    description: `Internship is one of the most desirable employment options as a college student. But
+    where do we begin to look for that sweet internship? How do CUNY students navigate and
+    utilize their resources to get internships? What are some of the trade offs and downsides? How
+    likely are we able to get an internship?`,
+  },
+]
